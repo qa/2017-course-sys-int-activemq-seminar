a2e5151c683c0455b858006758c0c94b5d2d315d
Jakub Knetl <jknetl@redhat.com>
2017-02-23 14:22:05 +0100

    Implement exercise 7 (MQTT)

--- src/main/java/com/redhat/brq/integration/activemq/MqttProducer.java
+++ src/main/java/com/redhat/brq/integration/activemq/MqttProducer.java
@@ -3,7 +3,17 @@
  */
 package com.redhat.brq.integration.activemq;
 
+import java.net.URISyntaxException;
+import java.util.Random;
+
 import javax.jms.JMSException;
+
+import org.fusesource.mqtt.client.BlockingConnection;
+import org.fusesource.mqtt.client.MQTT;
+import org.fusesource.mqtt.client.QoS;
+
+import com.redhat.brq.integration.activemq.util.XmlConverter;
+import com.redhat.brq.integration.activemql.model.Job;
 
 /**
  * Producer which uses MQTT protocol to communicate with ActiveMQ broker.
@@ -27,5 +37,38 @@ public class MqttProducer {
 	 * @throws JMSException
 	 */
 	public void produceMessages(int count) {
+		BlockingConnection connection = null;
+		try {
+			MQTT mqtt = new MQTT();
+			mqtt.setUserName("admin");
+			mqtt.setPassword("admin");
+			mqtt.setHost(brokerUrl);
+
+			connection = mqtt.blockingConnection();
+			connection.connect();
+
+			for (int i = 0; i < count; i++) {
+				Random random = new Random(System.currentTimeMillis());
+				int duration = random.nextInt(Job.MAX_DURATION) + 1;
+				Job job = new Job("Job " + (i + 1), duration);
+				String xml = XmlConverter.toXml(Job.class, job);
+				connection.publish(destinationName, xml.getBytes("UTF-8"), QoS.EXACTLY_ONCE, false);
+				System.out.println("Producer: sending: " + job.toString() + " to destination " + destinationName + " using MQTT protocol.");
+
+			}
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				try {
+					connection.disconnect();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 }
